0b360e70dfd0a5043621df83f1beb0cbb82b6b9f
Aziz Nasirov <nasirovaziz@yahoo.com>
2020-02-18 03:49:45 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,5 @@
-def reverse_each_word(phrase)
-  words_array = phrase.split(" ")
+def reverse_each_word(sentence)
+  words_array = sentence.split(" ")
   new_array = []
   new_array = words_array.collect {|word| word.reverse }
   new.array.join(" ")
